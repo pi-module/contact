a44602427d2150c508c8587a2b2defc326f52764
Marco <marcd@nordnet.fr>
2017-01-25 00:03:53 +0100

    bump module version

--- config/module.php
+++ config/module.php
@@ -15,7 +15,7 @@ return array(
     'meta'         => array(
         'title'         => _a('Contact'),
         'description'   => _a('Manage Contacts'),
-        'version'       => '1.3.1',
+        'version'       => '1.3.2',
         'license'       => 'New BSD',
         'logo'          => 'image/logo.png',
         'readme'        => 'docs/readme.txt',
